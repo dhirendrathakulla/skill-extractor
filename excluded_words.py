abc721e3cd1f2958d2b784d9da8982fd4bb61997
dhirendra <dhiren.thakulla@gmail.com>
2025-08-08 13:55:13 -0400

    setup ner

---
+++ excluded_words.py
@@ -0,0 +1,77 @@
+excluded_words = [
+       # Question words (interrogatives)
+    "who", "what", "when", "where", "why", "how", "which", "whom", "whose",
+
+    # Personal pronouns (subjective)
+    "i", "you", "he", "she", "it", "we", "they",
+
+    # Personal pronouns (objective)
+    "me", "him", "her", "us", "them",
+
+    # Possessive pronouns and determiners
+    "my", "your", "his", "her", "its", "our", "their",
+
+    # Reflexive pronouns
+    "myself", "yourself", "himself", "herself", "itself", "ourselves", "yourselves", "themselves",
+
+    # Demonstrative pronouns and determiners
+    "this", "that", "these", "those",
+
+    # Indefinite pronouns
+    "anyone", "anybody", "anything", "each", "either", "everyone", "everybody", "everything",
+    "neither", "nobody", "none", "no one", "nothing", "one", "other", "others", "someone", "somebody", "something",
+
+    # Quantifiers and determiners
+    "all", "both", "few", "many", "several", "some", "such", "enough", "more", "most", "less", "least", "much",
+
+    # Coordinating conjunctions
+    "and", "but", "or", "nor", "for", "yet", "so",
+
+    # Subordinating conjunctions
+    "although", "because", "since", "unless", "while", "whereas", "after", "before", "if", "once", "though", "until", "when", "whether",
+
+    # Common prepositions
+    "about", "above", "across", "after", "against", "along", "among", "around", "at", "before", "behind",
+    "below", "beneath", "beside", "between", "beyond", "by", "down", "during", "except", "for", "from", "in",
+    "inside", "into", "near", "of", "off", "on", "onto", "out", "outside", "over", "past", "since", "through",
+    "throughout", "to", "toward", "under", "underneath", "until", "up", "upon", "with", "within", "without",
+
+    # Particles and fillers
+    "again", "also", "just", "only", "even", "still", "yet", "too", "very", "really", "quite", "rather", "almost", "nearly", "well", "much",
+
+    # Negations
+    "no", "not", "none", "never", "nor",
+
+    # Others
+    "own", "same", "such", "any", "both", "each", "either", "another", "somehow", "somewhere", "anywhere", "nowhere",
+
+    # Modal auxiliary verbs
+    "can", "could", "may", "might", "must", "shall", "should", "will", "would",
+
+    # Common auxiliary verbs (be, have, do forms)
+    "am", "is", "are", "was", "were", "be", "been", "being",
+    "have", "has", "had", "having",
+    "do", "does", "did", "doing",
+
+    # Common filler verbs (often noisy)
+    "say", "said", "says",
+    "go", "went", "gone",
+    "get", "got",
+    "make", "made",
+    "take", "taken",
+    "come", "came", "coming",
+    "see", "saw", "seen",
+    "know", "knew", "known",
+    "think", "thought",
+    "look", "looked", "looking",
+    "use", "used",
+    "find", "found",
+    "tell", "told",
+    "ask", "asked",
+    "work", "worked",
+    "seem", "seemed",
+    "feel", "felt",
+    "try", "tried",
+    "leave", "left",
+    "call", "called"
+]
